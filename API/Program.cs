44da0b4cccded6036f4f6b465e0fbd2fa1ffe78c
MauroDoneda <mauro.doneda@gmail.com>
2022-12-08 20:57:38 +0100

    Add serilog and update configs

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,8 @@
 using DataAccess;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.SystemConsole.Themes;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +14,20 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDataAccess();
 
+// Use Serilog
+builder.Host.UseSerilog((hostContext, services, configuration) =>
+{
+    configuration
+        .MinimumLevel.Information()
+        .Enrich.FromLogContext()
+        .Enrich.WithThreadId()
+        .Enrich.WithThreadName()
+        .Enrich.WithEnvironmentName()
+        .Enrich.WithMachineName()
+        .WriteTo.File("logs/portolioApp.txt", rollingInterval: RollingInterval.Day, outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff}] [{Level:u3}] [{ThreadId}] [{SourceContext}] - {Message} {NewLine}{Exception}")
+        .WriteTo.Console(theme: AnsiConsoleTheme.Code, outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff}] [{Level:u3}] [{ThreadId}] [{SourceContext}] - {Message} {NewLine}{Exception}");
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,6 +35,20 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+var logger = services.GetRequiredService<ILogger<Program>>();
+try
+{
+    logger.LogInformation("Initiating db migration...");
+    var context = services.GetRequiredService<AppDbContext>();
+    await context.Database.MigrateAsync();
+}
+catch (Exception e)
+{
+    logger.LogError($"An error occured on migrating the db: {e.Message}");
 }
 
 app.UseHttpsRedirection();
