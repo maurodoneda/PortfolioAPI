521235e3bd5cd7407ba04591d290b7f1d4de533d
MauroDoneda <mauro.doneda@gmail.com>
2022-12-08 12:57:53 +0100

    Add settlement props

--- Domain/Entities/Settlement.cs
+++ Domain/Entities/Settlement.cs
@@ -1,6 +1,16 @@
+using Domain.Enums;
+
 namespace Domain.Entities;
 
 public class Settlement
 {
-    
+    public int SettlementId { get; set; }
+    public decimal TotalAmount { get; set; }
+    public decimal Adjustment { get; set; }
+    public decimal TradingFee { get; set; }
+    public decimal CustodyFee { get; set; }
+    public decimal Tax { get; set; }
+    public TransactionType TransactionType { get; set; }
+    public Asset Asset { get; set; }
+    public User User { get; set; }
 }
---
+++ Domain/Enums/TransactionType.cs
@@ -0,0 +1,8 @@
+namespace Domain.Enums;
+
+public enum TransactionType
+{
+    Undefined,
+    Credit,
+    Debit
+}
